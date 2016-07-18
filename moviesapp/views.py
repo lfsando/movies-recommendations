6e563b4f33ff95aa0cbfa3fb8833d53a228c1529
Luiz Domingues <lfsando@gmail.com>
2016-07-18 02:50:12 -0300

    Modiefied templates to display movies

--- moviesapp/views.py
+++ moviesapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Movie
+from django.utils import timezone
 
 
 # Create your views here.
 def movies_list(request):
-    return render(request, 'moviesapp/movie.html', {})
+    movies = Movie.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'moviesapp/movie.html', {'movies': movies})
