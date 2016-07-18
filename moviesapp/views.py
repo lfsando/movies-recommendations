274c9e356b45393b9467075d6a08b9bccd54bb1d
Luiz Domingues <lfsando@gmail.com>
2016-07-18 02:57:45 -0300

    Changed movie variable to be able to show every movie in views

--- moviesapp/views.py
+++ moviesapp/views.py
@@ -5,5 +5,6 @@ from django.utils import timezone
 
 # Create your views here.
 def movies_list(request):
-    movies = Movie.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #movies = Movie.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    movies = Movie.objects.all()
     return render(request, 'moviesapp/movie.html', {'movies': movies})
